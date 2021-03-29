aad6dff34a5f3facc7a4b9b7602af4efbd2ae462
Rosano Coutinho <pub@xeari.com>
2021-03-29 11:32:14 -0300

    OLSKPostinstallExternalAssets

--- package-postinstall.js
+++ package-postinstall.js
@@ -10,3 +10,15 @@
 		});
 	});
 })();
+
+(function OLSKPostinstallExternalAssets() {
+	require('./node_modules/OLSKExpress/modules/OLSKAssets/main.js').OLSKAssetsCopyAssetsFromTo([
+		'normalize.css',
+		'OLSKDecor',
+		'OLSKRouting',
+		'OLSKUIAssets',
+		'launchlet',
+		'ROCORootLink',
+		'webnative',
+	], require('path').join(__dirname, 'node_modules'), require('path').join(__dirname, 'os-app/_shared/__external'));
+})();
