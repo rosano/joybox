b3b03af9496fa7c8d487765c18ba8a7fef33cd68
Rosano Coutinho <pub@xeari.com>
2021-03-29 09:27:42 -0300

    Scaffold tech-robots

---
+++ os-app/tech-robots/controller.js
@@ -0,0 +1,18 @@
+exports.OLSKControllerRoutes = function() {
+	return [{
+		OLSKRoutePath: '/robots.txt',
+		OLSKRouteMethod: 'get',
+		OLSKRouteSignature: 'JOXRobotsRoute',
+		OLSKRouteFunction (req, res, next) {
+			return res.send(require('OLSKRobots').OLSKRobotsTXT([
+				'JOXVitrineRoute',
+			].reduce(function (coll, item) {
+				return coll.concat(res.locals.OLSKCanonical(item)).concat((res.locals.OLSKRouteObjectFor(item).OLSKRouteLanguageCodes || []).map(function (e) {
+					return res.locals.OLSKCanonicalLocalized(item, {
+						OLSKRoutingLanguage: e,
+					});
+				}));
+			}, [])));
+		},
+	}];
+};
