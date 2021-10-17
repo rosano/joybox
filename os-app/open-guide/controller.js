0a775038360b2b58d35b4439727efb5a110668f9
Rosano Coutinho <pub@xeari.com>
2021-10-16 21:46:29 -0400

    Hyphenate text basename

--- os-app/open-guide/controller.js
+++ os-app/open-guide/controller.js
@@ -5,7 +5,7 @@ exports.OLSKControllerRoutes = function () {
 		OLSKRouteSignature: 'JBXGuideRoute',
 		OLSKRouteFunction(req, res, next) {
 			return res.OLSKExpressLayoutRender(require('path').join(__dirname, 'ui-view'), {
-				JBXGuideContent: res.OLSKMarkdownContent(require('path').join(__dirname, `text.${ res.locals.OLSKSharedPageCurrentLanguage }.md`), Object.assign(require('OLSKDisk').OLSKDiskReadFile(require('path').join(__dirname, '../open-play/ui-logic.js')).split('\n').reduce(function (coll, item) {
+				JBXGuideContent: res.OLSKMarkdownContent(require('path').join(__dirname, `text-${ res.locals.OLSKSharedPageCurrentLanguage }.md`), Object.assign(require('OLSKDisk').OLSKDiskReadFile(require('path').join(__dirname, '../open-play/ui-logic.js')).split('\n').reduce(function (coll, item) {
 					const match = item.match(/const k(.*) = '(.*)';/);
 					return !match ? coll : Object.assign(coll, {
 						[match[1]]: match[2],
