e08fb843d921cef94f8b5ec3083e307f7afce844
Rosano Coutinho <pub@xeari.com>
2023-03-15 08:32:20 +0700

    Spec write error

--- os-app/open-play/main.svelte
+++ os-app/open-play/main.svelte
@@ -618,7 +618,13 @@ const mod = {
 	},
 
 	ZDRParamDispatchError (error) {
-		mod._ValueCloudErrorText = error.toString();
+		mod._ValueCloudErrorText = error.message;
+	},
+
+	ZDRParamDispatchWriteError (error) {
+		mod.ZDRParamDispatchError(error);
+
+		window.alert(error.message);
 	},
 
 	ZDRParamDispatchConnected (identity, token) {
@@ -721,6 +727,7 @@ const mod = {
 					],
 			}],
 			ZDRParamDispatchError: mod.ZDRParamDispatchError,
+			ZDRParamDispatchWriteError: mod.ZDRParamDispatchWriteError,
 			ZDRParamDispatchConnected: mod.ZDRParamDispatchConnected,
 			ZDRParamDispatchOnline: mod.ZDRParamDispatchOnline,
 			ZDRParamDispatchOffline: mod.ZDRParamDispatchOffline,
