72133eae5d7fb714d429765a751c68ca8b812577
Rosano Coutinho <pub@xeari.com>
2023-03-19 19:03:58 +0700

    Use project names for bulletin tag

--- os-app/open-vitrine/ui-test-_misc.js
+++ os-app/open-vitrine/ui-test-_misc.js
@@ -117,7 +117,7 @@ describe('JBXVitrine_Misc', function () {
 	describe('ROCOGazette', function test_ROCOGazette () {
 
 		it('sets ROCOBulletinProject', function () {
-			browser.assert.attribute('.ROCOBulletinProjectField', 'value', process.env.ROCO_SHARED_PROJECT_ID);
+			browser.assert.attribute('.ROCOBulletinProjectField', 'value', 'Joybox');
 		});
 
 	});
